25ea84a0705c269adcf9a276a95f44d2aa5fee85
novaTopFlex <78602536+novaTopFlex@users.noreply.github.com>
2024-04-18 09:21:40 -0400

    Create PythonMath.py

---
+++ PythonMath.py
@@ -0,0 +1,4 @@
+print("The absolute value of the number -1 is: " + str(abs(-1)))
+print("The binary interpretation of the decimal number 896 is: " + bin(896)))
+print("The binary interpretation of the octal number 777 is: " + bin(0o777))
+print("The binary intrepretation of the hexadecimal number face is: " + bin(0xface)))
