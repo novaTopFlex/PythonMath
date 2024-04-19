0fca078e97bbca63beb28ec35417e29264150e7c
novaTopFlex <78602536+novaTopFlex@users.noreply.github.com>
2024-04-18 21:18:29 -0400

    Update PythonMath.py

--- PythonMath.py
+++ PythonMath.py
@@ -2,3 +2,10 @@ print("The absolute value of the number -1 is: " + str(abs(-1)))
 print("The binary interpretation of the decimal number 896 is: " + bin(896))
 print("The binary interpretation of the octal number 777 is: " + bin(0o777))
 print("The binary intrepretation of the hexadecimal number face is: " + bin(0xface))
+print(str(bool(1)) + " should result in True.")
+print(str(bool(0)) + " should result in False.")
+print(str(bytes(7)) + " is intended to represent seven bytes.")
+print(callable(print)) # True
+print(callable(bytes)) # True
+print(callable(bytearray)) # True
+print(callable(0)) # False
